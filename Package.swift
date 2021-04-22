b8cf41feabfb4a6b97140455c1835ba1ed045cd9
Lars de Kroon <lars@ubiqu.com>
2021-04-22 16:48:46 +0200

    add binary data to xmlparsing

--- Package.swift
+++ Package.swift
@@ -18,11 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .binaryTarget(
             name: "XMLParsing",
-            dependencies: []),
-        .testTarget(
-            name: "XMLParsingTests",
-            dependencies: ["XMLParsing"]),
+            path: "xcframework/XMLParsing.xcframework")
     ]
 )
